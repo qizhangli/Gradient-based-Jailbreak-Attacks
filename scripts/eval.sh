392819c950545e09fd5255b616d7b9afebaa11a3
Qizhang Li <liqizhang95@gmail.com>
2024-07-01 10:41:16 +0800

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,10 @@
 
 
 if [[ "${logdir}" == *"gcgens"* ]]; then
-    python3 eval_generate.py --log_dir ${logdir}
+    python3 eval_generate.py --log_path ${logdir}
 else
-    python3 collect_response.py --log_dir ${logdir}
+    python3 collect_response.py --log_path ${logdir}
 fi
 
-python3 eval_harmbench.py --log_dir ${logdir}
+python3 eval_harmbench.py --log_path ${logdir}
 
